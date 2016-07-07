ddcbd5f890413524c4570024dcd581e2c7589964
kodak <gotomarryyou@gmail.com>
2016-07-07 09:14:54 +0800

    认证版本

--- app/src/main/java/com/kd/higit/ui/ReadFileActivity.java
+++ app/src/main/java/com/kd/higit/ui/ReadFileActivity.java
@@ -1,9 +1,11 @@
 package com.kd.higit.ui;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Base64;
+import android.view.View;
 import android.widget.TextView;
 
 import com.kd.gitnb.R;
@@ -14,6 +16,7 @@ import com.kd.higit.utils.Utils;
 
 import thereisnospon.codeview.CodeView;
 import thereisnospon.codeview.CodeViewTheme;
+import us.feras.mdv.MarkdownView;
 
 /**
  * Created by KD on 2016/6/24.
@@ -22,6 +25,7 @@ public class ReadFileActivity extends BaseSwipeActivity {
     private static final String TAG = ReadFileActivity.class.getSimpleName();
     private FileOrDirContent content;
     private CodeView file_content;
+    private MarkdownView markdownView;
     private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
@@ -44,6 +48,7 @@ public class ReadFileActivity extends BaseSwipeActivity {
         swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
         file_content = (CodeView) findViewById(R.id.codeview);
         file_content.setTheme(CodeViewTheme.DARKULA).fillColor();
+        markdownView = (MarkdownView) findViewById(R.id.markdownview);
     }
 
     @Override
@@ -67,7 +72,16 @@ public class ReadFileActivity extends BaseSwipeActivity {
     }
 
     private void updateFileContent() {
-        file_content.showCode(new String(Base64.decode(content.getContent(), Base64.DEFAULT)));
+        if (content.getName().endsWith(".md")) {
+            file_content.setVisibility(View.GONE);
+            markdownView.setVisibility(View.VISIBLE);
+            markdownView.loadUrl(content.getHtml_url());
+        } else {
+            file_content.setVisibility(View.VISIBLE);
+            markdownView.setVisibility(View.GONE);
+            file_content.showCode(new String(Base64.decode(content.getContent(), Base64.DEFAULT)));
+        }
+
     }
 
     private void requestFileContent(String url) {
